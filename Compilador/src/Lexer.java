870d886230156252f4d74ae6c9bfecd432e323a7
Carlos Antônio <carlosaajunior.jp@gmail.com>
2019-09-15 20:55:23 -0300

    Ajustes dos tokens para a matriz part 1

--- Compilador/src/Lexer.java
+++ Compilador/src/Lexer.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Lexer {
-    private ArrayList tokens;
+    private IMatrizDeSimbolos matriz;
     private int line;
     private int tamAtual;
     private ArrayList tokenAtual;
 
     public Lexer() {
-        this.tokens = new ArrayList<Token>();
         this.line = 0;
         this.tamAtual = 0;
         this.tokenAtual = new ArrayList();
@@ -37,30 +36,32 @@ public class Lexer {
             if (aLinha == '#'){
                 break;
             } else{
-            if ((aLinha == ' ') || (aLinha == '(') || (aLinha == ')') || (aLinha == '{') || (aLinha == '}')
-                    || (aLinha == ';') ||  (aLinha == ',') || aLinha == '\n') {
+                if ((aLinha == ' ') || (aLinha == '(') || (aLinha == ')') || (aLinha == '{') || (aLinha == '}')
+                        || (aLinha == ';') ||  (aLinha == ',') || aLinha == '\n') {
                     char[] tokenPego = new char[this.tamAtual];
                     for (int j = 0; j < this.tamAtual; j++) {
                         tokenPego[j] = this.tokenAtual.get(j).toString().charAt(0);
                     }
                     String tokenPegoString = String.copyValueOf(tokenPego);
-                    Token tokenExcept = new Token((Character.toString(aLinha)), "<" + Character.toString(aLinha) + ", ", this.line, Character.toString(aLinha));
                     if (this.tamAtual > 0) {
                         String tipo = identificarLexema(tokenPegoString);
-                        Token tokenFinal = new Token(tokenPegoString, tipo, this.line, tokenPegoString);
-                        this.tokens.add(tokenFinal);
+                        String pos = matriz.getLinhaAtual() + "," + matriz.getColunaAtual();
+                        Token tokenFinal = new Token(tokenPegoString, tipo, this.line, pos);
+                        this.matriz.alocaToken(tokenFinal);
                         this.tokenAtual.clear();
                         this.tamAtual = 0;
                     }
                     if (aLinha != ' ') {
-                        this.tokens.add(tokenExcept);
+                        String pos = matriz.getLinhaAtual() + "," + matriz.getColunaAtual();
+                        Token tokenExcept = new Token((Character.toString(aLinha)), "<" + Character.toString(aLinha) + ", ", this.line, pos);
+                        this.matriz.alocaToken(tokenExcept);
                     }
-            } else {
-                if(aLinha != '\n' && aLinha != ' ') {
-                    this.tokenAtual.add(aLinha);
-                    this.tamAtual++;
+                } else {
+                    if(aLinha != '\n' && aLinha != ' ') {
+                        this.tokenAtual.add(aLinha);
+                        this.tamAtual++;
+                    }
                 }
-            }
             }
         }
     }
@@ -122,21 +123,17 @@ public class Lexer {
             case ">":
                 return "< > operator, ";
 
-                default:
-                    if (tokenAtualIdentificado.charAt(0) == '0' || tokenAtualIdentificado.charAt(0) == '1' || tokenAtualIdentificado.charAt(0) == '2' ||
-                            tokenAtualIdentificado.charAt(0) == '3' || tokenAtualIdentificado.charAt(0) == '4' || tokenAtualIdentificado.charAt(0) == '5' ||
-                            tokenAtualIdentificado.charAt(0) == '6' || tokenAtualIdentificado.charAt(0) == '7' || tokenAtualIdentificado.charAt(0) == '8' ||
-                    tokenAtualIdentificado.charAt(0) == '9'){
-                        return "<integer constant, ";
-                    }else {
-                        return "<identifier, ";
-                    }
+            default:
+                if (tokenAtualIdentificado.charAt(0) == '0' || tokenAtualIdentificado.charAt(0) == '1' || tokenAtualIdentificado.charAt(0) == '2' ||
+                        tokenAtualIdentificado.charAt(0) == '3' || tokenAtualIdentificado.charAt(0) == '4' || tokenAtualIdentificado.charAt(0) == '5' ||
+                        tokenAtualIdentificado.charAt(0) == '6' || tokenAtualIdentificado.charAt(0) == '7' || tokenAtualIdentificado.charAt(0) == '8' ||
+                        tokenAtualIdentificado.charAt(0) == '9'){
+                    return "<integer constant, ";
+                }else {
+                    return "<identifier, ";
+                }
 
         }
 
-    }
-
-    public ArrayList<Token> getTokens() {
-        return this.tokens;
     }
 }
--- Compilador/src/Main.java
+++ Compilador/src/Main.java
@@ -8,9 +8,5 @@ public class Main {
         Lexer lex = new Lexer();
         //lex.lerCodigoFonte("C:\\Users\\carlo\\Documents\\Antônio-Adelino_Carlos-Antônio\\Comp\\Compilador\\src\\teste.txt");
         lex.lerCodigoFonte("/home/antonio/Documentos/Comp/Compilador/src/teste.txt");
-        ArrayList<Token> a = lex.getTokens();
-        for (int i = 0; i < a.size(); i++){
-            System.out.println(a.get(i).getLexema() + "" + a.get(i).getValor() + ">");
-        }
     }
 }
